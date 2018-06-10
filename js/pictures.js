ccf832354069971b6a5da30f5a43f0c6447b2d27
atinyakov <akoff16@gmail.com>
2018-06-10 16:32:51 +0300

    module3 done

--- js/pictures.js
+++ js/pictures.js
@@ -43,6 +43,49 @@ var fillProtosArray = function () {
 fillProtosArray();
 // console.log(photos);
 
-// var template = document.querySelector('#picture');
-// console.log(template);
-//test
+var template = document.querySelector('#picture');
+var imageTemplate = template.content.querySelector('.picture__link');
+var picturesBlock = document.querySelector('.pictures');
+
+var createElements = function () {
+  var fragment = document.createDocumentFragment();
+
+  for (var i = 0; i < photos.length; i++) {
+    var image = imageTemplate.cloneNode(true);
+    image.querySelector('.picture__img').src = photos[i].url;
+    image.querySelector('.picture__stat--likes').textContent = photos[i].likes;
+    image.querySelector('.picture__stat--comments').textContent = photos[i].comments.length;
+    fragment.appendChild(image);
+  }
+
+  picturesBlock.appendChild(fragment);
+};
+
+createElements();
+
+var bigPicture = document.querySelector('.big-picture');
+bigPicture.classList.remove('hidden');
+
+bigPicture.querySelector('.big-picture__img img').src = photos[0].url;
+bigPicture.querySelector('.likes-count').textContent = photos[0].likes;
+bigPicture.querySelector('.comments-count').textContent = photos[0].comments.length;
+bigPicture.querySelector('.social__caption').textContent = photos[0].description;
+var commentsBlock = bigPicture.querySelector('.social__comments');
+
+var addComments = function () {
+  var commentsFragment = document.createDocumentFragment();
+
+  for (var i = 0; i < photos[0].comments.length; i++) {
+    var commentTemplate = commentsBlock.querySelector('.social__comment').cloneNode(true);
+    commentTemplate.querySelector('.social__picture').src = 'img/avatar-' + generateRandomNumber(1, 6) + '.svg';
+    commentTemplate.querySelector('.social__text').textContent = photos[0].comments[i];
+    commentsFragment.appendChild(commentTemplate);
+  }
+  commentsBlock.innerHTML = '';
+  commentsBlock.appendChild(commentsFragment);
+};
+
+addComments();
+
+document.querySelector('.social__comment-count').classList.add('visually-hidden');
+document.querySelector('.social__loadmore').classList.add('visually-hidden');
