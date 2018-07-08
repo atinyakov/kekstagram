9f70347457fab6d25acbc76aa65cb003b15028b2
atinyakov <akoff16@gmail.com>
2018-07-08 19:37:55 +0300

    add startup filter

--- js/photos.js
+++ js/photos.js
@@ -5,13 +5,18 @@
   var template = document.querySelector('#picture');
   var imageTemplate = template.content.querySelector('.picture__link');
   var picturesBlock = document.querySelector('.pictures');
+  var imgFiltersBlock = document.querySelector('.img-filters');
+  var imgFilters = imgFiltersBlock.querySelector('.img-filters__form');
+  var imgFiltersButtons = imgFiltersBlock.querySelector('.img-filters__form');
   var ERROR_TIMEOUT = 4000;
   var COMMENTS_AMOUNT = 5;
 
-  var createElements = function (data) {
+  var createElements = function (data, amount) {
     var fragment = document.createDocumentFragment();
 
-    for (var i = 0; i < data.length; i++) {
+    amount = amount ? amount : data.length;
+
+    for (var i = 0; i < amount; i++) {
       var image = imageTemplate.cloneNode(true);
       image.querySelector('.picture__img').src = data[i].url;
       image.querySelector('.picture__stat--likes').textContent = data[i].likes;
@@ -26,7 +31,6 @@
 
   var addComments = function (data) {
     var commentsFragment = document.createDocumentFragment();
-
     for (var i = 0; i < COMMENTS_AMOUNT; i++) {
       var commentTemplate = commentsBlock.querySelector('.social__comment').cloneNode(true);
       commentTemplate.querySelector('.social__picture').src = 'img/avatar-' + window.utils.generateRandomNumber(1, 6) + '.svg';
@@ -50,13 +54,33 @@
     }, ERROR_TIMEOUT);
   };
 
+
   var onSuccess = function (data) {
     createElements(data);
     addComments(data);
+    imgFiltersBlock.classList.remove('img-filters--inactive');
     window.photos = {
       data: data
     };
   };
+
+  imgFilters.addEventListener('mouseup', function (evt) {
+
+    [].forEach.call(imgFiltersButtons, function (el) {
+      el.classList.remove('img-filters__button--active');
+    });
+
+    if (evt.target.id === 'filter-popular') {
+      createElements(window.photos.data);
+    } else if (evt.target.id === 'filter-new') {
+      createElements(window.photos.data, 10);
+    } else if (evt.target.id === 'filter-discussed') {
+      createElements(window.photos.data);
+    }
+
+    evt.target.classList.add('img-filters__button--active');
+  });
+
 
   window.load(URL, onSuccess, onError);
   window.photos = {
